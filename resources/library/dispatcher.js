3c33afbd21f35e27a6ba16ec19644fc34d6bb8d6
fengfengchenchen <2727224487@qq.com>
2021-09-06 09:32:05 +0800

    :bug:修复动画二刷bug

--- resources/library/dispatcher.js
+++ resources/library/dispatcher.js
@@ -1,65 +1,67 @@
-var Dispatcher = function() {
-	var d = {};
-	d.pages = [];
-	d.pageDict = {};
-	d.currentPage = -1;
-	d.status = "normal";
+var Dispatcher = function () {
+  var d = {};
+  d.pages = [];
+  d.pageDict = {};
+  d.currentPage = -1;
+  d.status = "normal";
 
-	d.loadPage = function(i) {
-		if(this.currentPage >= 0) {
-			var page = this.pages[this.currentPage];
-			var newPage = this.pages[i];
-			var that = this;
-			page.exit();
-			this.status = "pending";
-			page.addEventListener("exitEnd", function() {
-				newPage.enter();
-				page.removeEventListener("exitEnd");
-			});
-			newPage.addEventListener("enterEnd", function() {
-				that.status = "normal";
-				newPage.removeEventListener("enterEnd");
-			});
-		} else {
-			this.pages[i].init();
-			this.pages[i].enter();
-		}
-		if(i < this.pages.length - 1) {
-			this.pages[i+1].init(); //提前缓存下一个 
-		}
-		this.currentPage = i;
-	}
+  d.loadPage = function (i) {
+    if (this.currentPage >= 0) {
+      var page = this.pages[this.currentPage];
+      var newPage = this.pages[i];
+      var that = this;
+      page.exit();
+      this.status = "pending";
 
-	d.nextPage = function() {
-		if(this.status != "normal") {
-			return;
-		}
-		
-		var nextPage = this.currentPage;
-		if(this.currentPage == this.pages.length - 1) {
-			nextPage = 0;
-		} else {
-			nextPage++;
-		}
+      page.addEventListener("exitEnd", function () {
+        page.removeEventListener("exitEnd");
+        newPage.init();
+        newPage.enter();
+      });
 
-		this.loadPage(nextPage);
-	}
+      newPage.addEventListener("enterEnd", function () {
+        that.status = "normal";
+        newPage.removeEventListener("enterEnd");
+      });
+    } else {
+      this.pages[i].init();
+      this.pages[i].enter();
+    }
+    this.currentPage = i;
+  };
 
-	d.init = function() {
-		var pages = document.querySelectorAll("#app>.page");
-		for(var i = 0; i < pages.length; i++) {
-			if(pages[i].id != '') {
-				this.pageDict[pages[i].id] = i;
-			}
-			this.pages.push(Page.instance(pages[i]));
-		}
-	}
+  d.nextPage = function () {
+    if (this.status != "normal") {
+      return;
+    }
 
-	d.queryPage = function(id) {
-		return this.pageDict.hasOwnProperty(id) ? this.pages[this.pageDict[id]] : null;
-	}
+    var nextPage = this.currentPage;
+    if (this.currentPage == this.pages.length - 1) {
+      nextPage = 0;
+    } else {
+      nextPage++;
+    }
 
-	d.init();
+    this.loadPage(nextPage);
+  };
 
-	return d;
-}
+  d.init = function () {
+    var pages = document.querySelectorAll("#app>.page");
+    for (var i = 0; i < pages.length; i++) {
+      if (pages[i].id != "") {
+        this.pageDict[pages[i].id] = i;
+      }
+      this.pages.push(Page.instance(pages[i]));
+    }
+  };
+
+  d.queryPage = function (id) {
+    return this.pageDict.hasOwnProperty(id)
+      ? this.pages[this.pageDict[id]]
+      : null;
+  };
+
+  d.init();
+
+  return d;
+};
